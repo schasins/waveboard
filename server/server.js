5e11bc093c3b14a7866b42e78630a1d3254218db
Noura <culurienneldoreth@gmail.com>
2015-03-11 22:10:20 -0700

    handle post requests, save data to a file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var socketio = require('socket.io');
+var bodyParser = require('body-parser');
 
 var processFile = function(err, test_data) {
     if (err) {
@@ -28,14 +29,29 @@ fs.readFile('server/total_acc_x_truncated.txt', 'utf8', processFile);
 fs.readFile('server/total_acc_y_truncated.txt', 'utf8', processFile);
 fs.readFile('server/total_acc_z_truncated.txt', 'utf8', processFile);
 
-// starting servers
+// SERVERS
 var express_app = express();
+
+// static file server
 var p = path.join(__dirname, '../client');
 console.log("Serving: ",p);
 express_app.use(express.static(p));
-var express_server = express_app.listen(8000);
-var socketio_server = socketio.listen(express_server);
 
+// handle post requests of data from arduino
+express_app.use(bodyParser.urlencoded({ extended: false }));
+express_app.post('/data', function(req, res) {
+    console.log('got post with request body', req.body);
+    fs.appendFile('server/data.txt', JSON.stringify(req.body) + '\n\n', function(err) {
+        if (err) throw err;
+        console.log('appended to data.txt');
+    });
+});
+
+// starts the static file server and post requests handler
+var express_server = express_app.listen(8000);
+
+// socketio server to stream data to the client
+var socketio_server = socketio.listen(express_server);
 socketio_server.on('connect', function(socket) {
     console.log('client connected');
     socketio_server.emit('data', test_data_arrays);
