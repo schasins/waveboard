93b5c5eb5421afc0f1085cf7b0b13797014e6e7f
schasins <schasins@gmail.com>
2015-03-17 17:33:29 -0700

    Can generate semi-random streams of GPS data for testing.

--- client/map.js
+++ client/map.js
@@ -60,15 +60,46 @@ function newPosition(lat,long) {
 
 var testFuncCounter = 0;
 var latlngs = [[37.8757181,-122.2590485],[37.8757251,-122.2591496],[37.8757051,-122.2592507],[37.8757101,-122.2593518],[37.8757191,-122.2594529],[37.8757151,-122.2595530],[37.8757251,-122.2596541],[37.8757051,-122.2597552],[37.8757201,-122.2598552],[37.8757251,-122.2599552],[37.8757351,-122.2600552],[37.8757451,-122.2601552],[37.8757551,-122.2602552]];
-function testFunc(){
+
+function testFuncOld(){
   newPosition(latlngs[testFuncCounter][0],latlngs[testFuncCounter][1]);
 
   testFuncCounter += 1;
   if (testFuncCounter>=latlngs.length){
     return;
   }
-  setTimeout(testFunc, 2000);
+  setTimeout(testFuncOld, 3000);
 }
+
+function addJitter(base,max_jitter){
+  var neg = Math.random();
+  var mult = 1;
+  if (neg > .5){mult = -1;}
+  return base + mult*Math.random()*max_jitter;
+}
+// road by Soda
+/*
+var curr_ideal = [37.8757181,-122.2590485];
+var curr = curr_ideal;
+var lat_adjust = .00002;
+var long_adjust = -.000003;
+var max_jitter = .000008;
+*/
+// invention lab
+var curr_ideal = [37.874747, -122.258651];
+var curr = curr_ideal;
+var lat_adjust = 0;
+var long_adjust = 0;
+var max_jitter = .000001;
+function testFunc(){
+  newPosition(curr[0],curr[1]);
+  curr_ideal[0] = curr_ideal[0]+lat_adjust;
+  curr_ideal[1] = curr_ideal[1]+long_adjust;
+  curr[0] = addJitter(curr_ideal[0],max_jitter);
+  curr[1] = addJitter(curr_ideal[1],max_jitter);
+  setTimeout(testFunc,3000);
+}
+
 var test = true;
 if (test){
   setTimeout(testFunc,3000);
